2516b2bad74d9d293417d8963aa3f62c94161a76
BanyacskiG <126481196+BanyacskiG@users.noreply.github.com>
2023-05-14 22:02:32 +0200

    nemtom

--- js/Load.js
+++ js/Load.js
@@ -1,23 +1,8 @@
-window.onload = function(){
-    //navbar betöltése
-    var navbarHTML = localStorage.getItem('navbar')
-    document.getElementById('navbarload').innerHTML = navbarHTML
-    //.replace(/href="htmls\/egyeb.html"/g, 'href="egyeb.html"')
-    //.replace(/href="index.html"/g, 'href="index.html"')
-
-
-    //footer betöltése
-    var footerHTML = localStorage.getItem('footer')
-    document.getElementById('footerload').innerHTML = footerHTML
+function NavfooterLoad(){
+  //navbar betöltése
+  let navbarHTML = localStorage.getItem('navbar')
+  document.getElementById('navbarload').innerHTML = navbarHTML
+  //footer betöltése
+  let footerHTML = localStorage.getItem('footer')
+  document.getElementById('footerload').innerHTML = footerHTML
 }
-
-//function replaceHandler(currentPage){
-   // var navbarHTML = localStorage.getItem('element')
-   // var Updatenavbarlinks = navbarHTML.replace(/href="([^"])"/g, 'href="'+ getAbsolutePath(currentPage) +'/$1"')
-   // document.getElementById('navbarload').innerHTML = Updatenavbarlinks
-//}
-//function getAbsolutePath(relativePath){
-  //  var Link = document.createElement('a')
- //   Link.href = relativePath;
- //   return Link.href
-//}
--- js/Saves.js
+++ js/Saves.js
@@ -1,12 +1,33 @@
 function SaveHandlerIndex(){
     //navbar mentése
-    var navbarsave = document.getElementById('navbarsave').innerHTML;
-    //navbarsave = navbarsave.replace(/href="egyeb.html"/g, 'href="htmls/egyeb.html"')
-    //navbarsave = navbarsave.replace(/href="index.html"/g, 'href="..\/index.html"')
+    let navbarsave = document.getElementById('navbarsave').innerHTML;
     localStorage.setItem('navbar',navbarsave)
     console.log('Sikeresen elmentette a navbart')
     //footer mentése
-    var footersave = document.getElementById('footersave').innerHTML;
+    let footersave = document.getElementById('footersave').innerHTML;
     localStorage.setItem('footer', footersave)
+    console.log('Sikeresen elmentette a footert')
 }
+const Basket = {}
+var listitems = [0,0,0,0,0,0,0,0,0]
+
+function ToBasket(element){
+    var ids = document.getElementById(element.id)
+    
+    if(document.getElementById('mogyiModalItem') == ids){
+        listitems[0] = document.getElementById('mogyi').innerHTML
+        Basket.mogyi = listitems[0]
+        localStorage.setItem('OnBasket', Basket.mogyi)
+
+        var container = document.getElementById('Cart')
+        var doll = document.createElement('div')
+        doll.classList.add('col-sm-4', 'mb-3', 'd-flex', 'align-items-stretch')
+        container.appendChild(doll)
+        doll.innerHTML = localStorage.getItem('OnBasket')
+    }
+}
+
+
+
+
 
